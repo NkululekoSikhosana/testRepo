b816f192539a518c186be924f3138abda1c606af
NkululekoSikhosana <1135124@students.wits.ac.za>
2020-07-03 00:12:17 +0200

    adding more stuff

--- index.js
+++ index.js
@@ -1,49 +1,19 @@
-let path = require('path');
-let express = require('express');
-let app = express();
-let db = require('./db.js')
+const express = require('express')
+const mainRouter = require('./app/routes/mainRoutes.js')
+const bodyParser = require('body-parser')
+const path = require('path')
 
-app.get('/', function(req, res){
-res.sendFile(path.join(__dirname, 'index.html'));
-});
+const app = express()
 
-app.get('/database', function (req, res) {
-// Make a query to the database
-db.pools
-// Run query
-.then((pool) => {
-return pool.request()
-// This is only a test query, change it to whatever you need
-.query('SELECT 1')
-})
-// Send back the result
-.then(result => {
-res.send(result)
-})
-// If there's an error, return that with some description
-.catch(err => {
-res.send({
-Error: err
-})
-})
-})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// Get from database as JSON
-app.get('/api/index', (req, res) => {
-  const sqlQuery = `SELECT * FROM dbo.Bills`
-  db.pools
-      .then((pool) => {
-        return pool.request()
-          .query(sqlQuery)
-      })
-      .then(result => {
-        res.json(result.recordset)
-      })
-      .catch(err => {
-        res.json({ Error: err })
-      })
-})
+app.set('views', path.join(__dirname, './app/views'))
 
-let port = process.env.PORT || 3000;
-app.listen(port);
-console.log("Express server running on port", port);
+app.use('/static', express.static(path.join(__dirname, 'app/public')))
+app.use('/', mainRouter)
+
+const port = process.env.PORT || 3000
+app.listen(port)
+
+console.log('Express server running on port', port)
